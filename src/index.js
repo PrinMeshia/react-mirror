6c043efe8e0aaa90e1bc59e7ad0ff4cb6d98b1e0
PrinMeshia <jonathanv33@gmail.com>
2018-10-17 13:15:40 +0200

    resolve conflict

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-=======
-import './index.css';
->>>>>>> b682124d1f2437bc2837856cb1bf357d3a3c287f
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
